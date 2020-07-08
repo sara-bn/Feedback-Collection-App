2a3a8a53c25d407c1ab9fcd905c06a66b38612ee
Sara <sbanaeirad@my.bcit.ca>
2020-07-08 16:06:10 -0700

    refractoring and add mongoose

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
+mongoose.connect(keys.mongoURL);
+
 const app = express();
 
-//new instance of GoogleStrategy will be created
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback',
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken', accessToken);
-			console.log('refreshToken', refreshToken);
-			console.log('profile', profile);
-		}
-	)
-);
-
-app.get(
-	'/auth/google',
-	passport.authenticate('google', {
-		scope: ['profile', 'email'],
-	})
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+require('./routes/authRoutes')(app);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
