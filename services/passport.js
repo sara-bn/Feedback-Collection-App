b3e1b3a6981eb130365f7afde79edd08cf518dd4
Sara <sbanaeirad@my.bcit.ca>
2020-07-08 16:53:26 -0700

    add user to database

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,8 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+const User = mongoose.model('users');
 
 //new instance of GoogleStrategy will be created
 passport.use(
@@ -11,9 +13,7 @@ passport.use(
 			callbackURL: '/auth/google/callback',
 		},
 		(accessToken, refreshToken, profile, done) => {
-			console.log('accessToken', accessToken);
-			console.log('refreshToken', refreshToken);
-			console.log('profile', profile);
+			new User({ googleId: profile.id }).save();
 		}
 	)
 );
