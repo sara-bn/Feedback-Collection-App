e59841bfad928e61e371be96557c5b309fcfcc52
Sara <sbanaeirad@my.bcit.ca>
2020-07-09 18:00:31 -0700

    Refractort with await/asynch

--- services/passport.js
+++ services/passport.js
@@ -4,19 +4,16 @@ const mongoose = require('mongoose');
 const keys = require('../config/keys');
 const User = mongoose.model('users');
 
-
 //set id
 passport.serializeUser((user, done) => {
 	done(null, user.id);
 });
 
-
 //get id
 passport.deserializeUser((id, done) => {
-	User.findById(id)
-	.then(user =>{
-		done(null, user)
-	})
+	User.findById(id).then((user) => {
+		done(null, user);
+	});
 });
 //new instance of GoogleStrategy will be created
 passport.use(
@@ -26,14 +23,14 @@ passport.use(
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback',
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({ googleId: profile.id }).save().then((user) => done(null, user));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			if (existingUser) {
+				done(null, existingUser);
+			} else {
+				const user = await new User({ googleId: profile.id }).save();
+				done(null, user);
+			}
 		}
 	)
 );
