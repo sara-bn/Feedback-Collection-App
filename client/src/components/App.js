fc2c095d586c4071b148dec87c502453c04d5e0d
Sara <sbanaeirad@my.bcit.ca>
2020-07-13 13:07:25 -0700

    Link is added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,10 +3,10 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import Landing from './Landing';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveryNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
 	componentDidMount() {
