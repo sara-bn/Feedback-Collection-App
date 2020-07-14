19d61a30e3cd2acd884eb640e0d0303d4e4f471c
Sara <sbanaeirad@my.bcit.ca>
2020-07-14 00:50:43 -0700

    survey model added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURL);
@@ -21,7 +22,9 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
